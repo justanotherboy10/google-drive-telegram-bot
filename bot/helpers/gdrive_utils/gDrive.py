7319171f25daa98db41bd2bd3df2eab16ccd2052
justanotherboy10 <89351009+justanotherboy10@users.noreply.github.com>
2021-09-11 16:51:14 +0700

    Update gDrive.py

--- bot/helpers/gdrive_utils/gDrive.py
+++ bot/helpers/gdrive_utils/gDrive.py
@@ -150,7 +150,7 @@ class GoogleDrive:
       filesize = humanbytes(os.path.getsize(file_path))
       body = {
           "name": filename,
-          "description": "Uploaded using @UploadGdriveBot",
+          "description": "KoreanAddict.net",
           "mimeType": mime_type,
       }
       body["parents"] = [self.__parent_id]
